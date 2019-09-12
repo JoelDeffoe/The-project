92546ad36da69db75b50dea962b17a0534cd5345
JoelDeffoe <joeldeffoe@gmail.com>
2019-09-12 17:02:55 -0400

    test unit secessfull

---
+++ CSharpProject/SimpleCalculator/SimpleCalculator.Test.Unit/InputConverterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class InputConverterTest
+    {
+        private readonly InputConverter _inputConverter = new InputConverter();
+        [TestMethod]
+        public void ConvertStringIntoDouble()
+        {
+            string inputConver = "5";
+
+            double convertNumber = _inputConverter.ConverterInputeToNumberic(inputConver);
+            Assert.AreEqual(5, convertNumber);
+        }
+       
+        
+
+    }
+}
